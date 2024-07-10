4ceadb9163e6a068b26fada69975afbc99030077
sneh-soni <ssneh20062003@gmail.com>
2024-07-10 11:48:44 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -11,7 +11,11 @@ import path from "path";
 dontenv.config();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://youtube-woad-one.vercel.app", "http://localhost:3000"],
+  })
+);
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use("/uploads", express.static(path.join("uploads")));
